ca6ca4477ee5e6b016a45c1de693c8c6c728b5e8
Muindo N. Gituku <ngeenegituku@gmail.com>
2024-04-18 18:30:47 -0400

    Initial Commit

---
+++ Travel Journal/Travel_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_JournalApp.swift
+//  Travel Journal
+//
+//  Created by Muindo Gituku on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct Travel_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
