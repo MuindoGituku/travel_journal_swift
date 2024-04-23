3088d2bc4fa02685110fa83cc06dcc742a2042cc
Muindo N. Gituku <ngeenegituku@gmail.com>
2024-04-23 08:34:35 -0400

    I think we did something

---
+++ Travel Journal/HighlightImagesDialog.swift
@@ -0,0 +1,18 @@
+//
+//  HighlightImagesDialog.swift
+//  Travel Journal
+//
+//  Created by Muindo Gituku on 2024-04-23.
+//
+
+import SwiftUI
+
+struct HighlightImagesDialog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HighlightImagesDialog()
+}
